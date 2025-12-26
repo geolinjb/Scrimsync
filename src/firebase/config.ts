01a8686117e0747cecfc1e8b34e8df649b115444
Geolin JB <geolinjb@gmail.com>
2025-12-26 09:32:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8007311800-441b0",
+  "appId": "1:685729983626:web:cc0573578d4ebed7791173",
+  "apiKey": "AIzaSyDQVjhC1vztJGiSd6iOWMpltorPgSKaS7g",
+  "authDomain": "studio-8007311800-441b0.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "685729983626"
+};
