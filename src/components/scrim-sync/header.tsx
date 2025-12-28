15fc3fe112ddff7e59ed03c1c82b23f8bf070a8b
Geolin JB <geolinjb@gmail.com>
2025-12-28 10:47:52 +0000

    BpA8qniZ03YttlnTR25nc6RrWrZ2

--- src/components/scrim-sync/header.tsx
+++ src/components/scrim-sync/header.tsx
@@ -72,19 +72,6 @@ export function Header() {
                 </div>
               </DropdownMenuLabel>
               <DropdownMenuSeparator />
-              <DropdownMenuLabel className="font-normal">
-                <div 
-                  className="flex flex-col space-y-2 cursor-pointer"
-                  onClick={copyToClipboard}
-                  title="Click to copy your UID"
-                >
-                  <p className="text-xs font-semibold uppercase text-muted-foreground">Your Admin UID</p>
-                  <p className="text-xs leading-none text-foreground break-all">
-                    {user.uid}
-                  </p>
-                </div>
-              </DropdownMenuLabel>
-              <DropdownMenuSeparator />
               <DropdownMenuItem onClick={handleSignOut}>
                 <LogOut className="mr-2 h-4 w-4" />
                 <span>Log out</span>
