e0bad60fa6c6e237b062ef409bda677e49bcb83c
Geolin JB <geolinjb@gmail.com>
2026-01-05 02:24:30 +0000

    Im very sure there is a way to manually access backend so i can implemen

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,5 @@
-const functions = require("firebase-functions/v2/https");
+const functions = require("firebase-functions/v2");
+const https = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
 const admin = require("firebase-admin");
 
@@ -8,10 +9,10 @@ const auth = admin.auth();
 // The hardcoded UID for the super admin.
 const SUPER_ADMIN_UID = "BpA8qniZ03YttlnTR25nc6RrWrZ2";
 
-exports.setAdminClaim = functions.https.onCall(async (request) => {
+exports.setAdminClaim = https.onCall(async (request) => {
   // Check if the user is authenticated
   if (!request.auth) {
-    throw new functions.https.HttpsError(
+    throw new https.HttpsError(
       "unauthenticated",
       "You must be logged in to perform this action."
     );
@@ -21,7 +22,7 @@ exports.setAdminClaim = functions.https.onCall(async (request) => {
   const targetUid = request.data.uid;
 
   if (typeof targetUid !== "string" || targetUid.length === 0) {
-    throw new functions.https.HttpsError(
+    throw new https.HttpsError(
       "invalid-argument",
       "The function must be called with a 'uid' argument."
     );
@@ -34,7 +35,7 @@ exports.setAdminClaim = functions.https.onCall(async (request) => {
 
     // Only allow an existing admin or the super admin to proceed
     if (!isAdmin && !isSuperAdmin) {
-      throw new functions.https.HttpsError(
+      throw new https.HttpsError(
         "permission-denied",
         "You do not have permission to perform this action."
       );
@@ -51,10 +52,10 @@ exports.setAdminClaim = functions.https.onCall(async (request) => {
     };
   } catch (error) {
     logger.error("Error setting admin claim:", error);
-    if (error instanceof functions.https.HttpsError) {
+    if (error instanceof https.HttpsError) {
       throw error;
     }
-    throw new functions.https.HttpsError(
+    throw new https.HttpsError(
       "internal",
       "An unexpected error occurred while setting the admin claim."
     );
